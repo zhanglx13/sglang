1bd0e7d3edb2213d06d6a9214714163a6f6b87bc
Ying Sheng <sqy1415@gmail.com>
2024-11-25 13:31:54 -0800

    in progress

--- test/srt/test_session_control.py
+++ test/srt/test_session_control.py
@@ -1,7 +1,7 @@
 """
 Usage:
 python3 -m unittest test_session_control.TestSessionControl.test_session_control
-python3 -m unittest test_session_control.TestSessionControl.test_session_control_vlm
+python3 -m unittest test_session_control.TestSessionControlVision.test_session_control
 """
 
 import unittest
@@ -162,6 +162,161 @@ class TestSessionControl(unittest.TestCase):
         print("outputs from normal queries:")
         print(outputs_normal)
         assert outputs_from_session == outputs_normal
+
+
+class TestSessionControlVision(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.model = "lmms-lab/llava-onevision-qwen2-0.5b-ov"
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        cls.process = popen_launch_server(
+            cls.model, cls.base_url, timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_child_process(cls.process.pid, include_self=True)
+
+    def test_session_control(self):
+        text_chunks = [
+            "<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n",
+            "<|im_start|>user\n<image>\nDescribe this image in a very short sentence.<|im_end|>\n<|im_start|>assistant\n",
+            "<|im_end|><|im_start|>user\n<image>\nIs this image same with the previous image? Answer yes or no.<|im_end|>\n<|im_start|>assistant\n",
+            "<|im_end|><|im_start|>user\n<image>\nIs this image same with the previous image? Answer yes or no.<|im_end|>\n<|im_start|>assistant\n",
+        ]
+        image_chunks = [
+            None,
+            "https://raw.githubusercontent.com/sgl-project/sglang/main/assets/logo.png",
+            "https://raw.githubusercontent.com/sgl-project/sglang/main/assets/logo_square.png",
+            "https://raw.githubusercontent.com/sgl-project/sglang/main/assets/logo_square.png",
+        ]
+        assert len(text_chunks) == len(image_chunks)
+        tokenizer = get_tokenizer(self.model)
+        text_input_ids = [tokenizer.encode(x) for x in text_chunks]
+
+        # 1. using session control
+        session_id = requests.post(
+            self.base_url + "/open_session",
+            json={"capacity_of_str_len": 1000},
+        ).json()
+        rid = None
+
+        first_rid = None
+        outputs_from_session = []
+        for i in range(len(text_input_ids)):
+            response = requests.post(
+                self.base_url + "/generate",
+                json={
+                    "input_ids": text_input_ids[i],
+                    "image_data": image_chunks[i],
+                    "session": [session_id, rid],
+                    "sampling_params": {
+                        "temperature": 0,
+                        "max_new_tokens": (
+                            16 if i > 0 else 0
+                        ),  # prefill only for the first chunk
+                    },
+                },
+            ).json()
+            rid = response["meta_info"]["id"]
+            print(response)
+            if i == 0:
+                first_rid = rid
+            if i > 0:
+                outputs_from_session.append(response["text"])
+
+        # # backtrack to the first request and regenerate
+        # response = requests.post(
+        #     self.base_url + "/generate",
+        #     json={
+        #         "input_ids": chunks_ids[-1],
+        #         "session": [session_id, first_rid],
+        #         "sampling_params": {
+        #             "temperature": 0,
+        #             "max_new_tokens": 16,
+        #         },
+        #     },
+        # ).json()
+        # outputs_from_session.append(response["text"])
+
+        # # query with a non-existing rid (the last one should be disappeared becuase of backtrack), should see abort
+        # response = requests.post(
+        #     self.base_url + "/generate",
+        #     json={
+        #         "input_ids": chunks_ids[-1],
+        #         "session": [session_id, rid],
+        #         "sampling_params": {
+        #             "temperature": 0,
+        #             "max_new_tokens": 16,
+        #         },
+        #     },
+        # ).json()
+        # assert response["meta_info"]["finish_reason"]["type"] == "abort"
+
+        # ret = requests.post(
+        #     self.base_url + "/close_session",
+        #     json={"session_id": session_id},
+        # )
+        # assert ret.status_code == 200
+
+        # # send a request to a closed session, should see abort
+        # response = requests.post(
+        #     self.base_url + "/generate",
+        #     json={
+        #         "input_ids": chunks_ids[-1],
+        #         "session": [session_id, first_rid],
+        #         "sampling_params": {
+        #             "temperature": 0,
+        #             "max_new_tokens": 16,
+        #         },
+        #     },
+        # ).json()
+        # assert response["meta_info"]["finish_reason"]["type"] == "abort"
+
+        # # 2. not use session control
+        # input_ids_first_req = None
+        # input_ids = []
+        # outputs_normal = []
+        # for i, chunk_ids in enumerate(chunks_ids):
+        #     input_ids += chunk_ids
+        #     response = requests.post(
+        #         self.base_url + "/generate",
+        #         json={
+        #             "input_ids": input_ids,
+        #             "sampling_params": {
+        #                 "temperature": 0,
+        #                 "max_new_tokens": (
+        #                     16 if i > 0 else 0
+        #                 ),  # prefill only for the first chunk
+        #             },
+        #         },
+        #     ).json()
+        #     if i > 0:
+        #         input_ids += tokenizer.encode(response["text"])[
+        #             1:
+        #         ]  # drop the bos token
+        #         outputs_normal.append(response["text"])
+        #     if i == 0:
+        #         input_ids_first_req = input_ids.copy()
+
+        # input_ids_first_req += chunks_ids[-1]
+        # response = requests.post(
+        #     self.base_url + "/generate",
+        #     json={
+        #         "input_ids": input_ids_first_req,
+        #         "sampling_params": {
+        #             "temperature": 0,
+        #             "max_new_tokens": 16,
+        #         },
+        #     },
+        # ).json()
+        # outputs_normal.append(response["text"])
+
+        # print("outputs from chunked queries with session control:")
+        # print(outputs_from_session)
+        # print("outputs from normal queries:")
+        # print(outputs_normal)
+        # assert outputs_from_session == outputs_normal
 
 
 if __name__ == "__main__":
